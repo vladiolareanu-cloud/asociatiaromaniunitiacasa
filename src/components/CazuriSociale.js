d923bc6a370d8a026a2ee1106c4c9b72d911b4c2
Vlad Olareanu <vladiolareanu@gmail.com>
2026-01-20 13:35:55 +0200

    Update CazuriSociale.js

--- src/components/CazuriSociale.js
+++ src/components/CazuriSociale.js
@@ -1,155 +1,189 @@
-import { useEffect, useState } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay } from "swiper/modules";
-import "swiper/css";
-import jsonData from "@/data/cazuri.json";
+import Layout from "@/layout/Layout";
+import PageBanner from "@/layout/PageBanner";
 import VideoPopupLocal from "@/components/popup/VideoPopupLocal";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { Autoplay } from "swiper/modules";
+import jsonData from "@/data/cazuri.json";
 
 const CazuriSociale = () => {
-  const [cazuri, setCazuri] = useState([]);
-
-  useEffect(() => {
-    setCazuri(jsonData);
-  }, []);
-
   return (
-    <section className="cazuri_sociale">
-      <div className="container">
-        {cazuri.map((campanie, campIndex) => (
-          <div key={campIndex}>
-            {/* Titlu campanie */}
-            <div style={{ textAlign: "center", margin: "50px 0" }}>
-              <h2
+    <Layout pageName="Cazuri Sociale">
+      <PageBanner
+        pageName="Cazuri Sociale"
+        imageURL="img/hero/head_about_background.png"
+      />
+
+      {/* Video sub titlu */}
+      <VideoPopupLocal
+        videoSrc="/img/MAAS/MAAS_movie.mp4"
+        trigger={
+          <div className="neoh_fn_video">
+            <div
+              className="bg_overlay"
+              style={{
+                width: "100%",
+                paddingTop: "56.25%", // 16:9 aspect ratio
+                position: "relative",
+                borderRadius: "10px",
+                overflow: "hidden",
+              }}
+            >
+              <video
+                className="bg_video"
+                src="/img/MAAS/MAAS_movie.mp4"
+                autoPlay
+                loop
+                muted
+                playsInline
+                preload="metadata"
                 style={{
-                  color: "#00bfff",
-                  fontSize: "36px",
-                  fontWeight: "bold",
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "cover",
+                  cursor: "pointer",
+                }}
+              />
+            </div>
+            <div className="v_content">
+              <img
+                src="svg/play.svg"
+                alt="Play"
+                className="fn__svg"
+                style={{ cursor: "pointer" }}
+              />
+            </div>
+          </div>
+        }
+      />
+
+      {/* TOATE campaniile din JSON */}
+      {jsonData.map((campanie, campIndex) => (
+        <div key={campIndex}>
+          {/* Titlu campanie */}
+          <div style={{ textAlign: "center", margin: "50px 0" }}>
+            <h2
+              style={{
+                color: "#00bfff",
+                fontSize: "36px",
+                fontWeight: "bold",
+              }}
+            >
+              {campanie.titlu}
+            </h2>
+          </div>
+
+          {/* Cazuri */}
+          <div className="cazuri_sociale_wrapper">
+            {campanie.cazuri.map((item, index) => (
+              <div
+                key={index}
+                className={`caz_block ${
+                  item.pozitie === "left" ? "left" : "right"
+                }`}
+                style={{
+                  display: "flex",
+                  flexDirection:
+                    item.pozitie === "left" ? "row" : "row-reverse",
+                  alignItems: "center",
+                  marginBottom: "50px",
+                  gap: "30px",
+                  flexWrap: "wrap",
                 }}
               >
-                {campanie.titlu}
-              </h2>
-            </div>
-
-            {/* Cazuri */}
-            <div className="cazuri_sociale_wrapper">
-              {campanie.cazuri.map((item, index) => (
-                <div
-                  key={index}
-                  className={`caz_block ${item.pozitie === "left" ? "left" : "right"}`}
-                  style={{
-                    display: "flex",
-                    flexDirection: item.pozitie === "left" ? "row" : "row-reverse",
-                    alignItems: "center",
-                    marginBottom: "50px",
-                    gap: "30px",
-                    flexWrap: "wrap",
-                  }}
-                >
-                  {/* MEDIA: IMG sau VIDEO */}
-                  {item.poza.endsWith(".mp4") ? (
-                    <VideoPopupLocal
-                      videoSrc={item.poza}
-                      trigger={
-                        <div
-                          className="neoh_fn_video"
-                          style={{
-                            width: "45%",
-                            maxWidth: "400px",
-                            borderRadius: "10px",
-                            cursor: "pointer",
-                          }}
-                        >
-                          <div className="bg_overlay">
-                            <video
-                              src={item.poza}
-                              muted
-                              loop
-                              playsInline
-                              autoPlay
-                              style={{
-                                width: "100%",
-                                height: "100%",
-                                objectFit: "cover",
-                                borderRadius: "10px",
-                              }}
-                            />
-                          </div>
-                          <div className="v_content">
-                            <img
-                              src="/svg/play.svg"
-                              alt="Play"
-                              className="fn__svg"
-                            />
-                          </div>
-                        </div>
-                      }
-                    />
-                  ) : (
-                    <img
+                {/* MEDIA: IMG sau VIDEO */}
+                {item.poza.endsWith(".mp4") ? (
+                  <div
+                    style={{
+                      width: "45%",
+                      maxWidth: "400px",
+                      borderRadius: "10px",
+                      overflow: "hidden",
+                      backgroundColor: "#000",
+                      position: "relative",
+                      paddingTop: "56.25%", // 16:9 aspect ratio
+                    }}
+                  >
+                    <video
                       src={item.poza}
-                      alt={`Poza ${index}`}
+                      controls
+                      playsInline
+                      preload="metadata"
                       style={{
-                        width: "45%",
-                        maxWidth: "400px",
-                        borderRadius: "10px",
+                        position: "absolute",
+                        top: 0,
+                        left: 0,
+                        width: "100%",
+                        height: "100%",
                         objectFit: "cover",
                       }}
                     />
-                  )}
-
-                  {/* Text */}
-                  <p
+                  </div>
+                ) : (
+                  <img
+                    src={item.poza}
+                    alt={`Poza ${index}`}
                     style={{
-                      width: "50%",
-                      fontSize: "18px",
-                      lineHeight: "1.6",
-                      textAlign: "justify",
+                      width: "45%",
+                      maxWidth: "400px",
+                      borderRadius: "10px",
+                      objectFit: "cover",
                     }}
-                  >
-                    {item.text}
-                  </p>
-                </div>
-              ))}
+                  />
+                )}
 
-              {/* Slider imagini */}
-              {campanie.slider && campanie.slider.length > 0 && (
-                <div style={{ marginTop: "50px" }}>
-                  <Swiper
-                    spaceBetween={10}
-                    slidesPerView={4}
-                    loop={true}
-                    modules={[Autoplay]}
-                    autoplay={{ delay: 0, disableOnInteraction: false }}
-                    freeMode={true}
-                    speed={5000}
-                    breakpoints={{
-                      320: { slidesPerView: 1, spaceBetween: 10 },
-                      480: { slidesPerView: 2, spaceBetween: 10 },
-                      640: { slidesPerView: 2, spaceBetween: 10 },
-                      768: { slidesPerView: 3, spaceBetween: 10 },
-                      1024: { slidesPerView: 4, spaceBetween: 10 },
-                    }}
-                  >
-                    {campanie.slider.map((img, key) => (
-                      <SwiperSlide key={key}>
-                        <img
-                          src={img}
-                          alt={`Slider ${key}`}
-                          style={{
-                            width: "100%",
-                            borderRadius: "10px",
-                          }}
-                        />
-                      </SwiperSlide>
-                    ))}
-                  </Swiper>
-                </div>
-              )}
+                <p
+                  style={{
+                    width: "50%",
+                    fontSize: "18px",
+                    lineHeight: "1.6",
+                    textAlign: "justify",
+                  }}
+                >
+                  {item.text}
+                </p>
+              </div>
+            ))}
+
+            {/* Slider – DOAR IMAGINI */}
+            <div style={{ marginTop: "50px" }}>
+              <Swiper
+                spaceBetween={10}
+                slidesPerView={4}
+                loop={true}
+                modules={[Autoplay]}
+                autoplay={{ delay: 0, disableOnInteraction: false }}
+                freeMode={true}
+                speed={5000}
+                breakpoints={{
+                  320: { slidesPerView: 1, spaceBetween: 10 },
+                  480: { slidesPerView: 2, spaceBetween: 10 },
+                  640: { slidesPerView: 2, spaceBetween: 10 },
+                  768: { slidesPerView: 3, spaceBetween: 10 },
+                  1024: { slidesPerView: 4, spaceBetween: 10 },
+                }}
+              >
+                {campanie.slider.map((img, key) => (
+                  <SwiperSlide key={key}>
+                    <img
+                      src={img}
+                      alt={`Slider ${key}`}
+                      style={{
+                        width: "100%",
+                        borderRadius: "10px",
+                      }}
+                    />
+                  </SwiperSlide>
+                ))}
+              </Swiper>
             </div>
           </div>
-        ))}
-      </div>
-    </section>
+        </div>
+      ))}
+    </Layout>
   );
 };
 
