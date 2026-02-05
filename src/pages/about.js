2b05f93d49462a330ffde373ed48b7ea2cd4666e
vladiolareanu-cloud <vladiolareanu@gmail.com>
2026-02-05 15:49:17 +0200

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -100,7 +100,7 @@ import VideoPopupLocal from "@/components/popup/VideoPopupLocal";
             <div className="neoh_fn_team">
               <ul className="team_list">
                 {TeamInfor.map((item, key) =>
-                  key < 11 && (
+                  key < 12 && (
                     <li className="team_item" key={key}>
                       <div className="t_item">
                         <div className="person_info">
@@ -147,7 +147,7 @@ Prin sprijin și implicare activă, diaspora devine o punte între experiența a
             <div className="neoh_fn_team">
               <ul className="team_list">
                 {TeamInfor.map((item, key) =>
-                  key > 10 && key < 18 && (
+                  key > 11 && key < 19 && (
                     <li className="team_item" key={key}>
                       <div className="t_item">
                         <div className="person_info">
